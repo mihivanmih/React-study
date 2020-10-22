0e733cb6e3cb1d80b82291d67a42dd26313a9c8f
Gusb <mihivanmih@gmail.com>
2020-10-22 18:47:08 +0300

    13 lesson

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const NavBar = () => {
+
+    return (
+        <section className="leftMenu">
+            <a href="">Profile</a> <br/>
+            <a href="">Messages</a><br/>
+            <a href="">News</a><br/>
+            <a href="">Music</a><br/>
+            <a href="">Settings</a><br/>
+        </section>
+    )
+
+}
+
+export default NavBar
